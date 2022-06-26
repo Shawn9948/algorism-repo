85abea0219d1a4b3bf4d1f7efb0fe1ccc7f0918b
ZionB <104377048+Zion9948@users.noreply.github.com>
2022-06-26 21:11:56 +0900

    주석달기 대소문자 바꾸기.java
    주석달았습니다.

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*.length 메서드로 풀기*/
+        /*char타입 배열에 문자열을 삽입하여 아스키코드로 
+         *대문자 소문자 변환시켜주기*/
+        
         Scanner sc = new Scanner(System.in);
         String word = sc.next();
 
@@ -13,14 +15,13 @@ public class Main {
         // char[] arr = word.toCharArray(); 한번에도 가능!
         
         //char타입의 배열 반복문 돌려서 대문자는 소문자로, 소문자는 대문자로 변경하기
-        for(int i =0 ; i<arr.length; i++){
-            if(arr[i]>96&&arr[i]<123)
+        for(int i =0 ; i < arr.length ; i++){
+            if(arr[i]>96 && arr[i]<123)
                 arr[i]= (char)(arr[i]-32);
             else {
                 arr[i]= (char)(arr[i]+32);
             }
         }
-
         System.out.println(arr);
     }
 }
