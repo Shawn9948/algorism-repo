fae789081e80eb8b82e17eee67677fa0cc1c3621
ZionB <104377048+Zion9948@users.noreply.github.com>
2022-06-26 22:37:44 +0900

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰.java
    숏코딩

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -6,33 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
         /*킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개*/
-        /*배열을 생성하여 값을 저장한뒤 입력값 -(빼기) 원래 있어야하는값을 해서
+        /*배열을 생성하여 값을 저장한뒤 원래 있었던 값 빼기 입력 값을 해서
          *각 인덱스 마다 얼마를 더하거나 빼야하는지 출력한다.  */
+        
         Scanner sc = new Scanner(System.in);
-        int K,Q,L,B,N,P;
-
-
-
-        int[] whitePiece = new int[6];
-        for(int i = 0; i<6; i++){
-            whitePiece[i]= sc.nextInt();
-
-            if(i<2)
-            whitePiece[i] = 1-whitePiece[i];
-            else if (i>=2 && i<5) {
-                whitePiece[i]= 2-whitePiece[i];
-            } else {
-                whitePiece[5] = 8 -whitePiece[5];
-            }
+        
+        //검은 피스 배열 생성
+        int[] blackPiece = {1, 1, 2, 2, 2, 8};
+        
+        //반복문으로 검은피스의 인덱스 빼기 입력값 하기
+        for (int i = 0; i < blackPiece.length; i++) {
+            blackPiece[i] -= sc.nextInt();
         }
-        for (int e:whitePiece
-             ) {
-            System.out.print(e+" ");
+        //검은 피스 배열 출력
+        for (int e : blackPiece
+        ) {
+            System.out.print(e + " ");
         }
     }
 }
-/*
-입력값 - 1 >0
-0
-<0
-결과값이 -이냐 +이냐 해서 지역변수에 저장하고 나중에 한번에 출력*/
