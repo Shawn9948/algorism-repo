03d3da09e3dd380a0f3ce0cbc2c07182f85d32ae
ZionB <104377048+Zion9948@users.noreply.github.com>
2022-07-09 19:14:36 +0900

    [Bronze III] Title: 최소, 최대, Time: 1720 ms, Memory: 319408 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,16 +1,22 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[sc.nextInt()];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        int N = sc.nextInt();
+        //배열 생성
+        int[] numArr = new int[N];
+        int max = -1000000;
+        int min = 1000000;
+        for (int i = 0; i < N; i++) {
+            numArr[i] = sc.nextInt();
+            if (max < numArr[i])
+                max = numArr[i];
+            if (min > numArr[i])
+                min = numArr[i];
         }
-        Arrays.sort(arr);
-        System.out.println(arr[0] + " " + arr[arr.length - 1]);
+
+        System.out.println(min + " " + max);
+
     }
 }
