3e9583b9be133a0f8a607ccca439a80c63ac950a
ZionB <104377048+Zion9948@users.noreply.github.com>
2022-07-15 02:12:05 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,23 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        String nums = br.readLine();
+        char [] number = nums.toCharArray();
 
-        //변수생성
-        int N = 0;
-        String K = "";
-
-        //입력값 받기
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.next();
-        sc.close();
-
-        char[] arr = K.toCharArray();
-
-        for (int i = K.length() - 1; i >= 0; i--) {
-            System.out.println(N * (arr[i] - '0'));
-        }
-        System.out.println(N * Integer.parseInt(K));
+        System.out.println(num*(number[2]-'0'));
+        System.out.println(num*(number[1]-'0'));
+        System.out.println(num*(number[0]-'0'));
+        System.out.println(num*Integer.parseInt(nums));
     }
 }
