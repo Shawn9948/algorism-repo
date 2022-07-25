7f8485c99a68e1d00f4b28e7dbbb167ac3d3cf90
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2022-07-25 12:05:42 +0900

    [Silver V] Title: 단어 정렬, Time: 1980 ms, Memory: 25436 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayList <String> arrayList = new ArrayList<>();
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if (arrayList.contains(str)) {
+                continue;
+            }
+            arrayList.add(str);
+        }
+//        System.out.println(arrayList);
+
+       Collections.sort(arrayList,new Comparator<String>() {
+           @Override
+           public int compare(String o1, String o2) {
+               if (o1.length() == o2.length()) {
+                   for (int i = 0; i < o1.length(); i++) {
+                       if (o1.charAt(i) != o2.charAt(i))
+                           return o1.charAt(i) - o2.charAt(i);
+                   }
+               }
+
+               return o1.length()- o2.length();
+           }
+       });
+        for(String e : arrayList){
+            System.out.println(e);
+        }
+    }
+}
