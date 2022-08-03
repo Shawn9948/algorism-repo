aac6a9f1feef077c58617e1e8b756b9032f906f0
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2022-08-03 21:30:04 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -8,7 +8,7 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         /*컬렉션 배열에 집어넣은 뒤 내림차순하여 iterator를 이용하여 출력한다.*/
-        List<Integer> list = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
         //입력값 받기
         String [] N = br.readLine().split("");
         //list에 Integer타입으로 집어넣는다.
