3bc469eaa4e0ccae1164377ee719ac8d397697b2
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2023-01-11 17:39:14 +0900

    [level 1] Title: 하샤드 수, Time: 1.12 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,16 +1,7 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = false;
-        int hashadNumber = 0;
+        int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();
+        return x % sum == 0;
         
-        String str = String.valueOf(x);
-        
-        for(int i = 0; i<str.length(); i++){
-            hashadNumber += str.charAt(i)-'0';
-        }
-        
-        if(x % hashadNumber == 0) answer = true;
-        
-        return answer;
     }
 }
