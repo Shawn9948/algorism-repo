677e96458a1fb2fefd9a3f854f18d706d2b54503
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2023-01-13 17:45:01 +0900

    [level 1] Title: 3진법 뒤집기, Time: 11.34 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,21 +1,14 @@
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        char[] ternaryArray = Integer.toString(n, 3).toCharArray();
+public int solution(int n) {
+        String a = "";
 
-        for (int i = 0; i < ternaryArray.length/2; i++) {
-            char swapStorage =ternaryArray[i];
-            ternaryArray[i]= ternaryArray[ternaryArray.length - i - 1];
-            ternaryArray[ternaryArray.length - i - 1] = swapStorage;
+        while(n > 0){
+            a = (n % 3) + a;
+            n /= 3;
         }
+        a = new StringBuilder(a).reverse().toString();
 
-        String semiResult = "";
 
-        for (int i = 0; i < ternaryArray.length; i++) {
-            semiResult += String.valueOf(ternaryArray[i]);
-        }
-
-        answer = Integer.parseInt(semiResult, 3);
-        return answer;
+        return Integer.parseInt(a,3);
     }
 }
