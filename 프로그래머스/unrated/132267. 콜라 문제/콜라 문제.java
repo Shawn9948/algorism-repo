fc767a65989e4d8759351ab747f75820bd245fb2
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2023-01-25 18:24:19 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int count = 0;
+        int remainderNum = 0;
+        
+        
+        while(n/a>=1){
+             
+            if(n%a != 0){
+            remainderNum = n%a;
+            }else remainderNum = 0;
+            
+            n /= a;
+            count += n*b;
+           
+            n = (n*b)+remainderNum;
+        }
+        answer = count;
+        return answer;
+    }
+}
