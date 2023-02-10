8babc42f0a91e167228c051a5c1b1c058d843f55
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2023-02-10 23:12:05 +0900

    Update 가위 바위 보.java

--- 인프런강의/Array/가위 바위 보.java
+++ 인프런강의/Array/가위 바위 보.java
@@ -6,14 +6,13 @@ public class Main {
     public String solution(String a, String b) {
         int intA = Integer.parseInt(a);
         int intB = Integer.parseInt(b);
+        String answer = "B";
+        if(intA == intB) answer = "D";
+        else if (intA == 1 && intB == 3) answer = "A";
+        else if (intA == 2 && intB == 1) answer = "A";
+        else if (intA == 3 && intB == 2) answer = "A";
 
-        if (intA == 1 && intB == 2) return "B";
-        else if (intA == 1 && intB == 3) return "A";
-        else if (intA == 2 && intB == 1) return "A";
-        else if (intA == 2 && intB == 3) return "B";
-        else if (intA == 3 && intB == 1) return "B";
-        else if (intA == 3 && intB == 2) return "A";
-        return "D";
+        return answer;
     }
 
     public static void main(String[] args) {
