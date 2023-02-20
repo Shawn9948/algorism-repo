04dd971b234e3b3ac29f605066cd61725b1d7b82
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2023-02-20 16:13:29 +0900

    Update 학급회장.java

--- 인프런강의/HashMap_TreeSet /학급회장.java
+++ 인프런강의/HashMap_TreeSet /학급회장.java
@@ -3,27 +3,21 @@ import java.util.*;
 public class Main {
 
     public String solution(String str1, String str2) {
+        String answer = "YES";
         Map<Character, Integer> map = new HashMap<>();
-        char[] firstArr = str1.toCharArray();
-        for (char x : firstArr) {
+
+        for (char x : str1.toCharArray()) {
             map.put(x, map.getOrDefault(x, 0) + 1);
         }
+
         for (char x : str2.toCharArray()) {
-            if (!map.containsKey(x)) {
-                return "NO";
-            }
+            if (!map.containsKey(x) || map.get(x) == 0) return "NO";
+            map.put(x, map.get(x) - 1);
         }
-        Map<Character, Integer> map2 = new HashMap<>();
-        for (char x : str2.toCharArray()) {
-            map2.put(x, map2.getOrDefault(x, 0) + 1);
-        }
-        for (Character key : map.keySet()) {
-            if (!map.get(key).equals(map2.get(key))) {
-                return "NO";
-            }
-        }
-        return "YES";
+
+        return answer;
     }
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
