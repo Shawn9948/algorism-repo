b7ce1f365d187cdf946c8238ad829b6601785cc2
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2023-02-20 15:59:51 +0900

    Update 학급회장.java

--- 인프런강의/HashMap_TreeSet /학급회장.java
+++ 인프런강의/HashMap_TreeSet /학급회장.java
@@ -2,33 +2,35 @@ import java.util.*;
 
 public class Main {
 
-    public char solution(int size, String str){
+    public String solution(String str1, String str2) {
         Map<Character, Integer> map = new HashMap<>();
-
-        char[] arr = str.toCharArray();
-        for(char x : arr){
-            map.put(x, map.getOrDefault(x,0)+1);
+        char[] firstArr = str1.toCharArray();
+        for (char x : firstArr) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
-        Set<Character> set = map.keySet();
-        Iterator<Character> iter = set.iterator();
-        char answer = ' ';
-        int max = 0;
-        while(iter.hasNext()){
-            char target = iter.next();
-            if(max < map.get(target)){
-                max = map.get(target);
-                answer = target;
+        for (char x : str2.toCharArray()) {
+            if (!map.containsKey(x)) {
+                return "NO";
             }
         }
-        return answer;
+        Map<Character, Integer> map2 = new HashMap<>();
+        for (char x : str2.toCharArray()) {
+            map2.put(x, map2.getOrDefault(x, 0) + 1);
+        }
+        for (Character key : map.keySet()) {
+            if (!map.get(key).equals(map2.get(key))) {
+                return "NO";
+            }
+        }
+        return "YES";
     }
 
-    public static void main(String[] args){
-        Scanner in=new Scanner(System.in);
-        int input1 = in.nextInt();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input1 = in.next();
         String input2 = in.next();
         Main main = new Main();
         System.out.println(main.solution(input1, input2));
-        return ;
+        return;
     }
 }
