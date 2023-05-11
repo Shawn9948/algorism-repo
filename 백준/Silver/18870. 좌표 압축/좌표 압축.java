d18224bee288eb188e2b53ad543400285ed6df21
zion <104377048+SionBackEnd@users.noreply.github.com>
2023-05-11 19:19:03 +0900

    [Silver II] Title: 좌표 압축, Time: 1824 ms, Memory: 251036 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int size = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            set.add(i);
+        }
+
+        int[] array = set.stream().mapToInt(a -> a).toArray();
+        Arrays.sort(array);
+        Map<Integer, Integer> map = new HashMap<>();
+        int j =0;
+        for (Integer integer : array) {
+            Integer value = map.get(integer);
+            if (value != null) {
+                map.put(integer, value);
+            } else {
+                map.put(integer, j);
+                j++;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            arr[i] = map.get(arr[i]);
+        }
+        for (int i : arr) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
