d6f46650c3338329f744cb240733eecc10f62e5b
zion <104377048+SionBackEnd@users.noreply.github.com>
2023-06-18 18:36:56 +0900

    Update 매출액의 종류.java

--- 인프런강의/HashMap_TreeSet /매출액의 종류.java
+++ 인프런강의/HashMap_TreeSet /매출액의 종류.java
@@ -1,40 +1,48 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public String solution(int size, int range, int[] arr) {
+    private final Map<Integer, Integer> moneyCount = new HashMap<>();
+    public String solution(int n, int k, int[] arr){
         StringBuilder sb = new StringBuilder();
         int lp = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < range - 1; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-        }
-        for (int rp = range - 1; rp < size; rp++) {
-            map.put(arr[rp], map.getOrDefault(arr[rp], 0) + 1);
-            sb.append(map.size()).append(" ");
-            map.put(arr[lp], map.get(arr[lp]) - 1);
-            if (map.get(arr[lp]) == 0) map.remove(arr[lp]);
-            lp++;
+        put(arr[lp]);
+        int rp = lp+1;
+        while(rp < n){
+            put(arr[rp]);
+            if((rp-lp) == (k-1)) {
+                sb.append((long) moneyCount.keySet().size()).append(" ");
+                remove(arr[lp]);
+                lp++;
+            }
+            rp++;
         }
         return sb.toString();
+    }
+
+    private void put(int arr) {
+        moneyCount.put(arr, moneyCount.getOrDefault(arr, 0) + 1);
+    }
+    private void remove(int arr) {
+        Integer count = moneyCount.get(arr);
+        if(count > 1) moneyCount.put(arr, count-1);
+        else moneyCount.remove(arr);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int size = Integer.parseInt(st.nextToken());
-        int range = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        int[] arr = new int[size];
-        for (int i = 0; i < size; i++) {
+        for(int i = 0; i< n; i++)
             arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Main main = new Main();
-        System.out.println(main.solution(size, range, arr));
-        return;
+        Main m = new Main();
+        System.out.println(m.solution(n,k,arr));
     }
 }
