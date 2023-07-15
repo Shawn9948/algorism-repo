f2bfbf7a03ea2d1845044bc1e9a858ad3c7a324d
zion <104377048+SionBackEnd@users.noreply.github.com>
2023-07-15 16:37:04 +0900

    [level 2] Title: 주식가격, Time: 24.59 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i<prices.length; i++){
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                int pop = stack.pop();
+                answer[pop] = i- pop;
+            }
+            stack.push(i);
+        }
+        
+        while(!stack.isEmpty()){
+            int pop = stack.pop();
+            answer[pop] = prices.length-1 -pop;
+        }
+        return answer;
+    }
+}
