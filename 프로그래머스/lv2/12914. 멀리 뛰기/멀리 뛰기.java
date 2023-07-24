87b50304889e38bcc1c7199c3bf70b3562bedcc0
zion <104377048+SionBackEnd@users.noreply.github.com>
2023-07-24 16:44:18 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,11 +1,12 @@
+import java.util.*;
 class Solution {
     public long solution(int n) {
         long answer = 0;
+        //n이 1일 가능성이 있기 때문에 n+2로 초기화 진행
         long[] dp = new long[n+2];
         dp[1] = 1L;
         dp[2] = 2L;
-        
-        for(int i = 3; i< n+2; i++){
+        for(int i = 3; i< n+1; i++){
             dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
         }
         answer = dp[n];
