d940d86fc490e0585661a1e9fa168c3a24ae49c4
zion <104377048+SionBackEnd@users.noreply.github.com>
2023-08-24 11:17:59 +0900

    [Bronze I] Title: 평균, Time: 140 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,20 +1,33 @@
+
+import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int subject = sc.nextInt();
-        int [] Score = new int[subject];
-        for(int i = 0 ; i< subject; i++){
-           Score[i] = sc.nextInt();
-        }sc.close();
-        Arrays.sort(Score);
-        double average =0;
-        for (int k:Score
-             ) {
-           average += k/ (double)Score[subject-1]*100;
+     public double solution(int n, String m) {
+        double answer = 0;
+        long max = 1;
+        long total = 0;
+        long[] arr = Arrays.stream(m.split(" ")).mapToLong(Long::parseLong).toArray();
+        for (long l : arr) {
+            if(max < l)
+                max = l;
+            total += l;
         }
-        System.out.println(average/subject);
+
+        for (long l : arr) {
+            answer += (double)l / max * 100;
+        }
+
+        answer /= n;
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String m = br.readLine();
+
+        Main main = new Main();
+        System.out.println(main.solution(n, m));
     }
 }
