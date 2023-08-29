dc5e4bb5252f8ba0d0db69d80ff4e147248ab773
zion <104377048+SionBackEnd@users.noreply.github.com>
2023-08-29 11:45:16 +0900

    [Silver I] Title: 미로 탐색, Time: 200 ms, Memory: 17016 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,43 +1,59 @@
-import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
 
 public class Main {
-    static int answer = 0;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    static int[][] arr;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Main main = new Main();
+        int answer = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken()) + 2;
-        int M = Integer.parseInt(st.nextToken()) + 2;
-        int[][] arr = new int[N][M];
-        for (int i = 1; i < N - 1; i++) {
-            int[] array = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-            for (int j = 1; j < M - 1; j++)
-                arr[i][j] = array[j - 1];
+        arr = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
         }
-        boolean[][] ch = new boolean[N][M];
-        ch[1][1] = true;
+        int[] start = new int[]{0, 0, 1};
+        int[] end = new int[]{N - 1, M - 1, 1};
+
+        answer = bfs(start, end);
+
+
+        System.out.println(answer);
+    }
+
+    private static int bfs(int[] start, int[] end) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{1, 1});
+        queue.add(start);
+        boolean[][] ch = new boolean[arr.length][arr[0].length];
+        ch[start[0]][start[1]] = true;
+
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
+            int x = now[0];
+            int y = now[1];
+            int point = now[2];
+
+            if (x == end[0] && y == end[1]) {
+                return point;
+            }
+
             for (int i = 0; i < 4; i++) {
-                int x = now[0] + dx[i];
-                int y = now[1]+dy[i];
-                if (arr[x][y] != 0 && !ch[x][y]) {
-                    arr[x][y] = arr[now[0]][now[1]] + 1;
-                    ch[x][y] = true;
-                    queue.offer(new int[]{x, y});
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < arr.length && ny >= 0 && ny < arr[0].length && !ch[nx][ny]) {
+                    if (arr[nx][ny] != 0) {
+                        ch[nx][ny] = true;
+                        queue.offer(new int[]{nx, ny, point + 1});
+                    }
                 }
             }
         }
-        answer = arr[N - 2][M - 2];
-        System.out.println(answer);
+        return -1;
     }
 }
