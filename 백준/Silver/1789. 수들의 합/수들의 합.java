3ef577620de333c049bfdaf9dbec41f8425df729
zion <104377048+SionBackEnd@users.noreply.github.com>
2023-08-31 18:31:12 +0900

    [Silver V] Title: 수들의 합, Time: 136 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.java
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -1,31 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-class Main {
+
+public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long s = Long.parseLong(br.readLine());
+        int answer = 0;
+        int i = 1;
 
-        //N개수 입력
-        Long S = Long.parseLong(br.readLine());
-        //변수 값 초기화
-
-        int count = 0;
-        Long i = 1L;
-
-        while (true) {
-            //만약 s의 값이 i와 같다면 마지막으로 한번만 더 빼는게 가능하니 count++
-            if (S == i) {
-                count++;
-                break;
-            }//만약 s의 값이 i보다 작다면 더이상 뺄수없으니 그대로 출력
-            else if (S < i) {
-                break;
-            }
-            S -= i++;
-            count++;
-
+        while (s - i >= i + 1) {
+            s -= i++;
+            answer++;
         }
-        System.out.println(count);
+
+        if (s != 0) {
+            answer++;
+        }
+        System.out.println(answer);
     }
 }
